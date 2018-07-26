e484d2bf63a6a5a90987f845e3c888afee5f5327
Your Name Here <dane.spaeth@gmail.com>
2018-07-26 13:28:01 +0200

    Change Pointing Limit Plot

--- WaltzControl/coord_operations.py
+++ WaltzControl/coord_operations.py
@@ -220,5 +220,98 @@ def calc_obs_time(ha,dec):
             obs_time=ha_set-ha_now
             #Convert to solar time units
             return obs_time
+        
+def calc_tree_limit(az):
+    """Calculates tree limit in altitude for given azimuth.
+       Returns alt_limit in degrees.
+       Input: azimuth in degrees.
+    """
+    #Define Tree limits as np.array
+    #I put zero alt limit, where horizontal or cupboard limit is reached,
+    #because we do not care about tree limits below hard limits.
+    #Does not include all cupboard or horzontal areas, because it is zero there anyway
+    if not -180<=az<=180:
+        return False
+    
+    tree_lim=np.array([[0.0,-180.01],
+                       [0.0,-165.3455807696162],
+                       [13.926531858678072,-161.22484660697867],
+                       [17.195599636413682,-157.44910241374103],
+                       [0.0,-145.0],
+                       [0.0,-148.91114359009313],
+                       [21.58816304165209,-149.10471551491722],
+                       [12.182100707176437,-135.7205489225959],
+                       [17.29251687747958,-132.16310694376807],
+                       [17.358959391076436,-128.24795814317287],
+                       [16.554208994852967,-123.34078740486738],
+                       [13.011626593972498,-115.7274970740633],
+                       [0.0,-110.73615475166689],
+                       [0.0,-100.57064217069362],
+                       [0.0,-88.89096473867299],
+                       [12.767315026462386,-81.11510631225428],
+                       [13.63658755486348,-71.9033237952604],
+                       [13.730797953998692,-62.34671848645827],
+                       [16.517753594055026,-54.995932340824126],
+                       [15.385051933925672,-45.40736739783195],
+                       [14.249827605471754,-36.515993587901434],
+                       [17.244394510206345,-29.80225310600212],
+                       [16.786645206804543,-25.728955461859304],
+                       [19.385806016233353,-22.649468723617428],
+                       [17.815069758506976,-18.39429645277085],
+                       [13.917540178597369,-13.926187167552994],
+                       [15.800229806019255,-7.090540182345275],
+                       [14.402137910308108,0.0],
+                       [14.206429726562314,6.944851122938447],
+                       [13.917540178597369,13.926187167552998],
+                       [14.39736773524922,21.242902795231192],
+                       [18.693114258587876,26.36363462208435],
+                       [19.88987006933479,30.822460198427866],
+                       [17.742310373009012,38.16015634421578],
+                       [15.23857922621577,45.32492015300472],
+                       [19.331358903370177,52.23568016291085],
+                       [19.875106943741056,57.28528304962664],
+                       [25.903731403911603,66.86068454273801],
+                       [27.92145790178483,73.94757655442089],
+                       [25.482784869502435,82.40122417583163],
+                       [24.75519103243617,87.06221750457497],
+                       [21.78914070627903,89.41320012139184],
+                       [0.0,97.0],
+                       [0.0,101.22273624588037],
+                       [0.0,108.09973819537765],
+                       [0.0,112.96653118269231],
+                       [0.0,115.94832778139686],
+                       [21.796949268702594,124.046616191669],
+                       [0.0,119.6115621873876],
+                       [20.05071675877851,126.59684863727593],
+                       [0.0,124.02768286442313],
+                       [0.0,146.34084417895636],
+                       [0.0,180.01]])
+    
+    #We want to find the two defined limits nearest to given az
+    #Then we want to take the highest limit of the two
+
+    #Compute difference between limit azs an given az
+    diff=tree_lim[:,1]-az
+    #Take only positive and negative differences
+    pos=diff[diff>=0]
+    neg=diff[diff<0]
+    #print(diff)
+    #Find one limit at lowest positive value of difference in az
+    up_az_lim=tree_lim[np.where(diff==np.amin(pos))][0]
+    #The other at greatest negative value
+    low_az_lim=tree_lim[np.where(diff==np.amax(neg))][0]
+
+    #Take only altitude entries
+    up_alt_lim=up_az_lim[0]
+    low_alt_lim=low_az_lim[0]
+
+    #Get Maximum of the two altitude limits
+    #Maybe Minimum would be better for tree limits
+    alt_limit=max(up_alt_lim,low_alt_lim)
+    
+    return alt_limit
+
+    
+       
 
 
--- WaltzControl/plot_coords.py
+++ WaltzControl/plot_coords.py
@@ -1,48 +1,79 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-from coord_operations import calc_alt_limit, altaz_to_equ
+from coord_operations import calc_alt_limit, calc_tree_limit, equ_to_altaz, altaz_to_equ
 
-#Create az and alt_limit array
-az=np.arange(-180,180,0.05)
+#Create az and alt_limit and tree_limit array
+az=np.arange(-180,180,0.01)
 alt_limit=np.zeros(len(az))
+tree_limit=np.zeros(len(az))
 
-#Calculate alt_limit for every az
+#Calculate alt_limit for every az na tree_limit
 for index in range(len(az)):
     alt_limit[index]=calc_alt_limit(az[index])
+    tree_limit[index]=calc_tree_limit(az[index])
 
-#Plot alt_limits vs az    
+#Plot alt_limits vs az and tree limits  
 plt.plot(az,alt_limit,color='red')
+plt.plot(az,tree_limit,color='#00cc00',linewidth=0.0)
+plt.fill_between(az,alt_limit,np.zeros(len(az)),color='red',alpha=1)
+plt.fill_between(az,tree_limit,alt_limit,color='#00cc00',where=tree_limit>alt_limit,alpha=1)
 plt.axis([-180, 180, 0, 90])
 plt.xlabel('Azimuth[°]')
 plt.ylabel('Altitude[°]')
 plt.title('Waltz Pointing Limits')
 
 #Define star (later function input)
-star_az=50
-star_alt=30
+star_az=-120
+star_alt=35
+
+#Calculate star ha and dec
+star_ha,star_dec=altaz_to_equ(star_alt,star_az)
+#Define star trajectory (dec stays constant, hour angle increases)
+traj_ha=np.arange(star_ha,12,0.05)
+traj_dec=np.ones(len(traj_ha))*star_dec
+#Calculate star trajectory in alt and az
+traj_alt=np.zeros(len(traj_ha))
+traj_az=np.zeros(len(traj_ha))
+
+for index in range(len(traj_ha)):
+    traj_alt[index],traj_az[index],_,__=equ_to_altaz(traj_ha[index],traj_dec[index])
+    
 
 #plot star
 plt.plot(star_az,star_alt,'b*')
+plt.plot(traj_az,traj_alt,'b:')
 plt.show()
 
 #Define dec_limits and hour angle arrays
 dec_limit=np.zeros(len(az))
 ha=np.zeros(len(az))
 
-#Transodrm altaz limits to ha,dec limits
+#And tree limit array
+dec_tree_limit=np.zeros(len(az))
+ha_tree_limit=np.zeros(len(az))
+dec_zero_alt=np.zeros(len(az))
+#Transform altaz limits to ha,dec limits
 for index in range(len(az)):
     ha[index],dec_limit[index]=altaz_to_equ(alt_limit[index],az[index])
+    ha_tree_limit[index],dec_tree_limit[index]=altaz_to_equ(tree_limit[index],az[index])
+    __,dec_zero_alt[index]=altaz_to_equ(0,az[index])
     
 #Plot alt_limits vs az    
+plt.plot(ha_tree_limit,dec_tree_limit,color='#00cc00',linewidth=0.0)
 plt.plot(ha,dec_limit,color='red')
-plt.axis([-12, 12, -30, 90])
+y_plot_limit=-30
+
+plt.fill_between(ha_tree_limit,dec_tree_limit,np.ones(len(ha))*y_plot_limit,interpolate=True,color='#00cc00',where=dec_tree_limit>dec_limit,alpha=1)
+plt.fill_between(ha,dec_limit,np.ones(len(ha))*y_plot_limit,color='red',alpha=1)
+plt.axis([-12, 12, y_plot_limit, 90])
 plt.xlabel('Hour Angle[h]')
 plt.ylabel('Declination[°]')
 plt.title('Waltz Pointing Limits')
 
 #plot star
-#plt.plot(star_az,star_alt,'b*')
+plt.plot(star_ha,star_dec,'b*')
+plt.plot(traj_ha,traj_dec,'b:')
 plt.show()  
 
 
